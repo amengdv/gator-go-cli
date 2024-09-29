035db17f37b3d4a2a57c3212aed2696a0ddcf1c9
amengdv <aminkafri0082@gmail.com>
2024-09-29 11:53:25 +0800

    feat: few basic command

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/amengdv/gator/internal/config"
+	"github.com/amengdv/gator/internal/database"
+	_ "github.com/lib/pq"
+)
+
+
+func main() {
+
+    conf, err := config.Read()
+    if err != nil {
+        log.Fatalf("Failed to read config: %v\n", err)
+    }
+
+    db, err := sql.Open("postgres", conf.DbUrl)
+    if err != nil {
+        log.Fatalf("Failed to connect to a database: %v\n", err)
+    }
+
+    dbQueries := database.New(db)
+
+    s := state{
+        db: dbQueries,
+        cfg: &conf,
+    }
+
+    c := commands{
+        handlers: make(map[string]func(*state, command) error),
+    }
+
+    c.register("login", handlerLogin)
+    c.register("register", handlerRegister)
+    c.register("reset", handlerReset)
+    c.register("users", handlerUsers)
+    c.register("agg", handlerAgg)
+    c.register("addfeed", handlerAddFeed)
+    c.register("feeds", handlerFeeds)
+    c.register("follow", handlerFollow)
+
+    argsWithoutProg := os.Args[1:]
+
+    if len(argsWithoutProg) < 1 {
+        log.Printf("Require a command name")
+        os.Exit(1)
+    }
+
+    commandName := argsWithoutProg[0]
+    commandArgs := argsWithoutProg[1:]
+
+    err = c.run(&s, command{
+        commandName,
+        commandArgs,
+    })
+
+    if err != nil {
+        log.Printf("Error: %v\n", err)
+        os.Exit(1)
+    }
+}
